69b01dae65527861b947f90daed7daeef774bef9
Vlad Hrinchenko <vladuslavhrinchenko@gmail.com>
2025-07-19 18:45:06 +0300

    fix fix

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://notehub-api.goit.study',
+  baseURL: 'https://09-auth-two.vercel.app/',
   withCredentials: true,
 });
