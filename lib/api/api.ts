0ef1ef11ddf6423cc96f628f4b2e23027017d1a2
Vlad Hrinchenko <vladuslavhrinchenko@gmail.com>
2025-07-19 18:57:17 +0300

    olol

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,7 +1,7 @@
 // lib/api/api.ts
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 export const nextServer = axios.create({
   baseURL,
