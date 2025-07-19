6ab4125b0f57fefe2bda18d8c4f436ed37d28563
Vlad Hrinchenko <vladuslavhrinchenko@gmail.com>
2025-07-19 16:12:11 +0300

    fix

--- app/api/users/me/route.ts
+++ app/api/users/me/route.ts
@@ -1,7 +1,7 @@
 export const dynamic = "force-dynamic";
 
 import { NextResponse } from "next/server";
-import { api } from "@/app/api/api";
+import  api  from "@/lib/api/api";
 import { cookies } from "next/headers";
 
 export async function GET() {
